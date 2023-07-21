fc006fbe044b21b56b5a1d88fe5dc6faa7f819bf
Evrengo <136704312+Evrengo@users.noreply.github.com>
2023-07-21 17:32:20 +0300

    Add files via upload

---
+++ AspNetMvcBlog/App.Web.Mvc/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace App.Web.Mvc.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
